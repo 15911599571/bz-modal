c2ec60be9c07b6032995c5cafef70ffb0b4aafda
bigzhu <vermiliondun@gmail.com>
2016-07-16 23:17:22 +0800

    不要把依赖打包进去

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -83,6 +83,9 @@ module.exports = {
     loaders: utils.cssLoaders()
   },
   externals: {
-    jquery: 'jQuery'
+    jquery: 'jQuery',
+    'semantic-ui-dimmer': 'semantic-ui-dimmer',
+    'semantic-ui-transition': 'semantic-ui-transition',
+    'semantic-ui-modal': 'semantic-ui-modal'
   }
 }
